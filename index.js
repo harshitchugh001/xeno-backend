c835fa5cf6ff5aa663d32159afdfdc09a0444b33
Harshit <harshitchugh001@gmail.com>
2023-06-07 14:54:19 +0530

    serverless function

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 require('dotenv').config();
+
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connected'))
+  .catch((error) => {
+    console.log(`DB connection error: ${error.message}`);
+  });
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
-mongoose.set('strictQuery', true);
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB Connected"))
-    .catch((error) => {
-        console.log(`DB connection error: ${error.message}`);
-    });
-
-
-
-const authRoutes = require('./routes/auth');
-const coursesRoutes = require('./routes/courses');
-
 app.get('/', (req, res) => {
-    res.send('Hello from Node API');
+  res.send('Hello from Node API');
 });
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/api', authRoutes);
-app.use('/api', coursesRoutes);
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`API is running on port ${port}`);
-});
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/courses', require('./routes/courses'));
+
+
+module.exports = app;
