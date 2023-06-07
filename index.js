55930e6584702e2755fbf351b65452e1af168c9a
Harshit <harshitchugh001@gmail.com>
2023-06-07 11:59:46 +0530

    server

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const cors = require('cors');
+const serverless = require('serverless-http'); 
+
+const app = express();
 
 mongoose.set('strictQuery', true);
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"))
-  .catch((error) => {
-    console.log(`DB connection error: ${error.message}`);
-  });
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch((error) => {
+        console.log(`DB connection error: ${error.message}`);
+    });
 
 const authRoutes = require('./routes/auth');
 const coursesRoutes = require('./routes/courses');
 
-const { createServer } = require('serverless-http');
-const express = require('express');
-const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello from Node API');
+});
 
-app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(cors());
+
 app.use('/api', authRoutes);
 app.use('/api', coursesRoutes);
 
-module.exports = createServer(app);
+const port = process.env.PORT || 8000;
+
+module.exports.handler = serverless(app);
