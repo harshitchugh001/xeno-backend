9bcbd564860ef3043c2d82e96ffd65f7304f8b3f
Harshit <harshitchugh001@gmail.com>
2023-07-25 16:55:41 +0530

    serverless function

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const cors = require('cors');
 const app = express();
 
 mongoose.set('strictQuery', true);
+
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB Connected"))
-    .catch((error) => {
-        console.log(`DB connection error: ${error.message}`);
-    });
-
-
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((error) => {
+    console.log(`DB connection error: ${error.message}`);
+    process.exit(1);
+  });
 
 const authRoutes = require('./routes/auth');
-const contactRoutes=require('./routes/contact');
+const contactRoutes = require('./routes/contact');
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node API');
+  res.send('Hello from Node API');
 });
 
 app.use(bodyParser.json());
@@ -35,7 +35,7 @@ app.use('/api', contactRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port}`);
 });
 
-module.exports=app
+module.exports = app;
