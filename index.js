a0d10d07d59b4f02f0b14561585acca4b73571f0
Harshit <harshitchugh001@gmail.com>
2023-07-25 17:01:59 +0530

    index

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+require('dotenv').config();
 
 const app = express();
 
+// connect to db
 mongoose.set('strictQuery', true);
-
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"))
-  .catch((error) => {
-    console.log(`DB connection error: ${error.message}`);
-    process.exit(1);
-  });
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"));
 
+
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+// import routes
 const authRoutes = require('./routes/auth');
 const contactRoutes = require('./routes/contact');
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node API');
+    res.send('Hello from Node API');
 });
 
+// app middlewares
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cors()); // allows all origins
 
-app.use(cors());
 
+// middleware
 app.use('/api', authRoutes);
 app.use('/api', contactRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+    console.log(`API is running on port ${port}`);
 });
 
-module.exports = app;
+module.exports = app
